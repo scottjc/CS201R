a5eeef69f407cf691d8ebbe57acf5b761dbdd95b
scottjc <scottchristensen111@gamil.com>
2016-02-25 17:51:42 +0000

    Fixed the JSON for Project 4

--- Project4/foldername/routes/index.js
+++ Project4/foldername/routes/index.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var fs = require('fs');
+//make a request like http://ec2-52-10-93-201.us-west-2.compute.amazonaws.com:3000/getcity?q=Provo
+var express = require('express');//kill %1 stops the npm start you did.
+var fs = require('fs');//lets us read varous files
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('index.html', { root:  'public' });
+  res.sendFile('index.html', { root:  'public' });//or whatever your file name is in the public file
 
 });
 
@@ -23,13 +24,13 @@ router.get('/getcity',function(req,res,next) {
             for(var i = 0; i < cities.length; i++) {
               var result = cities[i].search(myRe);
           		if(result != -1) {
-            	 console.log(cities[i]);
-           		 jsonresult.push({city:cities[i]});
+            	 console.log(cities[i]);//display to console
+           		 jsonresult.push({city:cities[i]});//display to website
           		}
             }
 
             console.log(jsonresult);
-            res.status(200).json(jsonresult);
+            res.status(200).json(jsonresult);//display the results
     });
 });
 
